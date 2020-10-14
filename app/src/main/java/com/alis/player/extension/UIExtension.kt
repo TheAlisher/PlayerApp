350e9d49b7f1fcd550628456f58bf2f6ca4d90ae
macbookpro <abenov2005.uzer@gmail.com>
2020-10-14 23:16:56 +0600

    added -> method loadRoundImage

--- app/src/main/java/com/alis/player/extension/UIExtension.kt
+++ app/src/main/java/com/alis/player/extension/UIExtension.kt
@@ -3,11 +3,20 @@ package com.alis.player.extension
 import android.view.View
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 
 fun ImageView.loadImage(url: String? = null, placeholder: Int = 0) {
     Glide.with(context)
         .load(url)
         .placeholder(placeholder)
+        .into(this)
+}
+
+fun ImageView.loadRoundImage(url: String? = null, placeholder: Int = 0, roundedRadius: Int = 0) {
+    Glide.with(context)
+        .load(url)
+        .placeholder(placeholder)
+        .transform(RoundedCorners(roundedRadius))
         .into(this)
 }
 
